c45fa1332002b2f415fc27b5c70434f33f6aefdd
Danielxburns <danielburnsart@gmail.com>
2020-06-08 18:44:49 -0500

    debug db connection

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ console.log('inside server.app.get');
 
 app.post('/newTask', ((req, res) => {
   const task = req.body;
-  console.log('inside server.app.post "/newTask" req.body :>> ', req.body);
+  console.log('inside server.app.post "/newTask" req.body :>> ', req);
   db.postNewTask(task, (err, data) => {
     if(err) {
       res.status(500).send(err);
