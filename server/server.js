8952c76f8ca167a552b9426833e7f9c29a60fa8a
Danielxburns <danielburnsart@gmail.com>
2020-06-09 18:15:11 -0500

    add browserify

--- server/server.js
+++ server/server.js
@@ -7,11 +7,7 @@ const db = require('../db/index.js');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/', express.static(path.join(__dirname, '/../client')));
 
