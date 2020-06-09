61453703771b3ec542cffdf08014e5d5136db8c1
Danielxburns <danielburnsart@gmail.com>
2020-06-09 16:03:08 -0500

    debug connection to db

--- client/app.js
+++ client/app.js
@@ -140,7 +140,7 @@ async function postNewTask(subject, day, task) {
   };
   console.log('app.postNewTask config :>> ', config);
   const request = new Request(`${url}/newTask`, config)
-  console.log('app.postNewTask request.clone().headers:>> ', request.clone().headers);
+  console.log('app.postNewTask request.clone():>> ', request.clone());
   const response = await fetch(request);
   try {
     const taskData = await response.json();
