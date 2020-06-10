5d7301b7ec1db5ef4d92ab1a70669df40925e093
Danielxburns <danielburnsart@gmail.com>
2020-06-09 19:14:14 -0500

    function on deployed server

--- client/app.js
+++ client/app.js
@@ -33,7 +33,6 @@ function endWeek(day) {
 };
 
 function populateCells(tasks) {
-  console.log('inside populateCells tasks :>> ', tasks);
   cells.forEach(cell => {
     const subj = cell.className;
     const day = cell.parentNode.className;
